0a74bdc4fba84816b4e5619f0650d092a2049d73
Viduth04 <jmadheepa@gmail.com>
2026-01-19 14:44:10 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
-    proxy: {
-      // 👇 Anything starting with /api will go to your backend
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+    // Remove the proxy for production; we will use the 'api.js' instance instead
   },
 })
