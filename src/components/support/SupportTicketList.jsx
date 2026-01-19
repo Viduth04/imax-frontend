cd25bb8ca407f6eeb48ff8a9f8b57a7543615210
Viduth04 <jmadheepa@gmail.com>
2026-01-19 23:42:11 +0530

    fixed error in vercel

--- src/components/support/SupportTicketList.jsx
+++ src/components/support/SupportTicketList.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import api from '../../api.js';
+import api from '@/api';
 import toast from 'react-hot-toast';
 import { jsPDF } from 'jspdf';
 import autoTable from 'jspdf-autotable';
